b99ae7a025e9e4c38ed23932de50bddd18afd0c7
YY1113 <75839430+YY1113@users.noreply.github.com>
2021-01-13 22:31:34 +0900

    paint 브러쉬 기능 구현
    $ css와 별개로 paint 캔버스 사이즈도 설정해줘야함.
    $

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+/*CSS와 별개로 js에서 캔버스에 값이 들어갈 영역 설정을 따로 해줘야함 */
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = "2.5";
+
 
 let painting = false;
 
@@ -6,10 +15,20 @@ function onMouseMove(event) {
     /*스크린내 옵셋값 추출 */
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function stopPainting() {
     painting = false;
+}
+function startPainting() {
+    painting = true;
 }
 
 function onMouseDown(event) {
@@ -17,16 +36,11 @@ function onMouseDown(event) {
 
 }
 
-function onMouseUp(event) {
-    stopPainting();
-    
-}
-
 /*마우스 움직임 감지 */
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave" stopPainting);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
 
